f2ef6b51ad9bf8ec1d85b4219e81a4482ffb26cb
Paul Carvill <85612526+Eskimoodigital@users.noreply.github.com>
2021-07-22 09:57:01 +0200

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,64 @@
+
+
+
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+    region = "eu-central-1"
+}
+
+
+
+
+resource "aws_instance" "eskimoo16661" {
+    ami = "ami-00f22f6155d6d92c5"
+    instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.instance.id]
+
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello, World" > index.html
+                nohup busybox httpd -f -p 8080 &
+                EOF
+  
+    tags = {
+        Names = "eskimooexample"
+    }
+
+}
+
+
+
+resource "aws_security_group" "instance" {
+    name = "terraform-example-instance"
+
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+} 
+
